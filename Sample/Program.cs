4ea12d5adabb26afdd191d6c0a4f5e89fa2ea71d
Andrei Epure <andrei.epure@sonarsource.com>
2021-06-14 19:16:56 +0200

    Improve reproducer sample

--- Sample/Program.cs
+++ Sample/Program.cs
@@ -4,9 +4,25 @@ namespace Sample
 {
     public class Program
     {
-        public void Method(UnknownType argument)
+        public void Method(string first, UnknownType second)
         {
-            throw new ArgumentNullException(nameof(argument));
+            var localVar = "value";
+
+            // the values for c.SemanticModel.GetConstantValue(firstArgExpression);
+
+            new Foo("literal");                           // hasValue=True, Value="literal"
+            new Foo(1);                                   // hasValue=True, Value=1
+
+            new Foo(localVar);                            // hasValue=False
+            new Foo(known);                               // hasValue=False
+            new Foo(unknown);                             // hasValue=False
+
+            new Foo(nameof(localVar));                    // hasValue=True, Value="localVar"
+            new Foo(nameof(first));                       // hasValue=True, Value="first"
+            new Foo(nameof(second));                      // hasValue=True, Value="" (empty string)
+                                                          // EXPECTED: I would expect either hasValue to be False or Value to be "second"
         }
+
+        class Foo { public Foo(object o) { } }
     }
 }
